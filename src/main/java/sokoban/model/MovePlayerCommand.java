b788b009aea18d9a9dc2399e82106366cd1634d5
Marc Takoumba <mtakoumba@gmail.com>
2024-05-13 12:57:48 +0200

    fonctionnement de undo okay

--- src/main/java/sokoban/model/MovePlayerCommand.java
+++ src/main/java/sokoban/model/MovePlayerCommand.java
@@ -5,78 +5,65 @@ import javafx.collections.ObservableList;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class MovePlayerCommand implements Command{
     private final Board4Play board4Play;
+    private final Stack<List<CellState>> statesStack = new Stack<>();
 
-    public List<CellState> getCellStates() {
-        return cellStates;
-    }
-
-    private final List<CellState> cellStates = new ArrayList<>();
-
-    public MovePlayerCommand(Board4Play board4Play){
+    public MovePlayerCommand(Board4Play board4Play) {
         this.board4Play = board4Play;
     }
 
-
-
     @Override
     public void execute() {
-        // je dois faire une copie de l'etat actuelle de mes cells
+        // Je dois faire une copie de l'état actuel des cellules et l'ajouter à la pile
         saveState(board4Play.getGrid4Play());
     }
 
     @Override
     public void undo() {
-        // restore l'etat precedent
-        copyElementsToGrid();
+        // Restaure l'état précédent de la grille
+        if (!statesStack.isEmpty()) {
+            List<CellState> prevState = statesStack.pop();
+            restoreState(prevState);
+        }
     }
 
     @Override
     public void redo() {
-        //rexecute l'etat comme il etait
+        // Réexécute l'état comme il était (si nécessaire)
         execute();
     }
 
     private void saveState(Grid4Play grid4Play) {
-        cellStates.clear();
+        List<CellState> currentState = new ArrayList<>();
 
-        // Parcourir toutes les cellules de la grille
+        // Parcourt toutes les cellules de la grille
         for (int i = 0; i < grid4Play.getGridHeight(); i++) {
             for (int j = 0; j < grid4Play.getGridWidth(); j++) {
-                // Obtenir les éléments de la cellule actuelle
+                // Obtient les éléments de la cellule actuelle
                 ObservableList<Element> elements = FXCollections.observableArrayList(grid4Play.getCellsElements(i, j));
-
-                // Créer une copie des éléments
+                // Crée une copie des éléments
                 ObservableList<Element> elementsCopy = FXCollections.observableArrayList(elements);
-
-                // Créer un état de cellule et l'ajouter à la liste des états
-                cellStates.add(new CellState(grid4Play.getMatrix()[i][j], elementsCopy));
+                // Crée un état de cellule et l'ajoute à la liste des états
+                currentState.add(new CellState(grid4Play.getMatrix()[i][j], elementsCopy));
             }
         }
 
+        // Ajoute l'état actuel à la pile
+        statesStack.push(currentState);
     }
 
-
-
-    public void printState(){
-        for (CellState cellState : cellStates) {
-            System.out.println("Cell: " + cellState.getCell());
-            System.out.println("Elements: " + cellState.getElements());
-            System.out.println();
-        }
-    }
-
-    private void copyElementsToGrid() {
+    private void restoreState(List<CellState> prevState) {
+        // Restaure l'état précédent de la grille
         Grid4Play grid4Play = board4Play.getGrid4Play();
-        for (CellState cellState : cellStates) {
-            Cell4Play cell = cellState.getCell();
-            ObservableList<Element> elements = cellState.getElements();
-            // Efface les éléments actuels de la cellule
-            cell.getCellsElements().clear();
-            // Ajoute les éléments de la copie sauvegardée
-            cell.getCellsElements().addAll(elements);
+        for (int i = 0; i < grid4Play.getGridHeight(); i++) {
+            for (int j = 0; j < grid4Play.getGridWidth(); j++) {
+                Cell4Play cell = grid4Play.getMatrix()[i][j];
+                CellState cellState = prevState.get(i * grid4Play.getGridWidth() + j);
+                cell.getCellsElements().setAll(cellState.getElements());
+            }
         }
     }
 
