2a5f0617eaafedf679cae6f921733466f48d65e3
Marc Takoumba <mtakoumba@gmail.com>
2024-05-23 14:49:41 +0200

    augmentation de 20 mouvement lorsque le MushRoom est clické

--- src/main/java/sokoban/view/Cell4PlayView.java
+++ src/main/java/sokoban/view/Cell4PlayView.java
@@ -98,16 +98,13 @@ public class Cell4PlayView extends CellView<Cell4PlayViewModel> {
     }
 
 
-    private void handleClickCellPlay(){
-        this.setOnMouseClicked(event -> {
-            if (getCellViewModel().ClickOnMushroom()){
-                getCellViewModel().getBoard4Play().getGrid4Play().setMoveCount(getCellViewModel().getBoard4Play().getGrid4Play().getMoveCount() + 20);
-                getCellViewModel().getBoard4Play().getGrid4Play().moveBoxesToRandomEmptyCells();
-                getCellViewModel().getBoard4Play().getGrid4Play().removeMushroom();
-                getCellViewModel().getBoard4Play().getGrid4Play().getCell4Play().addMushroomToRandomEmptyCell();
-            }
-        });
-
+    public void handleClickCellPlay() {
+        if (getCellViewModel().ClickOnMushroom()) {
+            getCellViewModel().getBoard4Play().getGrid4Play().setMoveCount(getCellViewModel().getBoard4Play().getGrid4Play().getMoveCount() + 20);
+            getCellViewModel().getBoard4Play().getGrid4Play().moveBoxesToRandomEmptyCells();
+            getCellViewModel().getBoard4Play().getGrid4Play().removeMushroom();
+            getCellViewModel().getBoard4Play().getGrid4Play().getCell4Play().addMushroomToRandomEmptyCell();
+        }
     }
 
     private void handleKeyPressed(KeyEvent event) {
